d44986e465349b3ec850acaa2226a838261498bd
Diego Carvajal <dicarvajalb@unal.edu.co>
2023-10-04 18:55:09 -0500

    auth ms with graphql api

--- routes.js
+++ routes.js
@@ -1,23 +1,23 @@
 const routes = {
     apiFlight: {
       user: {
-        url: "http://localhost:8080/user",
+        url: "http://user:8080/user",
         route: "/apiUser",
       },
       notification: {
-        url: "http://localhost:3000/notifications",
+        url: "http://notification:3000/notifications",
         route: "/apiNotification",
       },
       gps: {
-        url: "http://localhost:3001/ubicacion",
+        url: "http://gps:3001/ubicacion",
         route: "/apiGPS",
       },
       msg: {
-        url: "http://localhost:8000/messages",
+        url: "http://message:8000/messages",
         route: "/apiMsg",
       },
       auth: {
-        url: "http://localhost:7049/api/Auth",
+        url: "http://authms:7049/api/Auth",
         route: "/apiAuth",
       },
     }
