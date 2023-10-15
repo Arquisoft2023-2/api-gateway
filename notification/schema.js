909eda3df2f41c8aa2e5b7aef46901831e47a30a
jyarac <jyarac@unal.edu.co>
2023-10-15 14:03:23 -0500

    correccion en esquema de notificacion

--- notification/schema.js
+++ notification/schema.js
@@ -4,7 +4,7 @@ import { buildSchema } from "graphql"
 
 const schema = buildSchema(`
     type Query {
-        getNotifications: [Notification]
+        getNotifications(id: String): [Notification]
         createNotification(plate : String, message: String): Boolean
         updateNotification(id: String, plate : String, message: String): Boolean
         deleteNotification(id: String): Boolean
